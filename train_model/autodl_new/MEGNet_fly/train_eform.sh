8fc42fc78ded54fd864da9a26d8a3d0e97d22470
log3me <5353465+log3me@user.noreply.gitee.com>
2026-01-01 16:16:59 +0800

    change fold

--- train_model/autodl_new/MEGNet_fly/train_eform.sh
+++ train_model/autodl_new/MEGNet_fly/train_eform.sh
@@ -10,7 +10,7 @@ max_per_gpu=7  #
 
 # 数据集和fold配置
 subset="matbench_mp_e_form"
-declare -a folds=("0" "1" "2" "3" "4")
+declare -a folds=("1" "2" "3" "4")  # ("0" "1" "2" "3" "4")
 
 # 参数组合
 declare -a combinations=(
@@ -177,3 +177,5 @@ done
 echo "=============================================="
 echo "All tasks for all folds finished!"
 echo "=============================================="
+
+# fold0 dim32 e1 f0.01
